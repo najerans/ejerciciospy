0fa006482633ca8d9aa1cba86c19ecd8ffc2df1c
najerans <norel@nsmn19.onmicrosoft.com>
2023-08-02 21:34:20 -0600

    Update ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -1,1 +1,16 @@
 print('Muere mundo')
+
+nombre = "kai del mal"
+numero = 56
+boleano = True
+print(type(nombre))
+print(nombre)
+print(type(numero))
+print(numero)
+print(type(boleano))
+print(boleano)
+
+
+import keyword    #libreria palabras reservadas
+print(keyword.kwlist) #lista de palabras reservadas completas 
+print(keyword.softkwlist)
